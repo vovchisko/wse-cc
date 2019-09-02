85840f34177f7fb66a35ffdbf65451ba79711ff3
Vladimir Ishenko <vovchisko@gmail.com>
2019-09-03 02:17:16 +0300

    mass-reformating

--- src/client.js
+++ src/client.js
@@ -1,38 +1,38 @@
-const { WseClient } = require('wse')
-const { _isf, _f } = require('./shared')
+const {WseClient} = require("wse");
+const {_isf, _f} = require("./shared");
 
 class WseCCClient extends WseClient {
     constructor(url, ws_params, protocol) {
-        super(url, ws_params, protocol)
+        super(url, ws_params, protocol);
 
-        this.on('message', (c, dat) => {
-            let f = _isf(c)
+        this.on("message", (c, dat) => {
+            let f = _isf(c);
             if (f) {
-                if (typeof this[f] === 'function') {
-                    this[f](dat)
+                if (typeof this[f] === "function") {
+                    this[f](dat);
                 } else {
-                    console.error('warning: unknown function ' + f, dat)
+                    console.error("warning: unknown function " + f, dat);
                 }
             }
-        })
+        });
 
-        this._payload = null
-        this.core = new WseClient('', ws_params, protocol)
+        this._payload = null;
+        this.core = new WseClient("", ws_params, protocol);
     }
 
     connect(payload, params) {
         // remember payload for reconnect
-        this._payload = payload
-        this._params = params
-        super.connect(this._payload, this._params)
+        this._payload = payload;
+        this._params = params;
+        super.connect(this._payload, this._params);
     }
 
     _lead(dat) {
-        if (this.core.is_online) this.core.close()
-        this.core.url = 'ws://' + dat.addr + '/ws-core'
-        this.core.connect(this._payload, this._params)
-        this.core.emit('lead', dat)
+        if (this.core.is_online) this.core.close();
+        this.core.url = "ws://" + dat.addr + "/ws-core";
+        this.core.connect(this._payload, this._params);
+        this.core.emit("lead", dat);
     }
 }
 
-module.exports = WseCCClient
+module.exports = WseCCClient;
